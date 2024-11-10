184e9da6048749405a64354a548ecb6f8d11bf2a
pangliang <418094911@qq.com>
2024-11-10 22:04:10 +0800

    参考charles原理, 使用自签名的根证书来动态签发域名tls证书

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,19 @@
 package cmd
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"log"
+	"math/big"
 	"net"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/kujourinka/mhws_beta_server/backend"
 	"github.com/spf13/cobra"
@@ -28,5 +39,79 @@ func mainRun(cmd *cobra.Command, args []string) {
 	}
 	e := backend.RegisterHandler()
 
-	e.RunTLS(args[0]+":443", "cert/website.crt", "cert/website.key")
+	subCert, err := createSelfSignedCert("cert/root.crt", "cert/root.key")
+	if err != nil {
+		log.Fatalf("createSelfSingedCert error: %v", err)
+	}
+
+	server := http.Server{
+		Addr:    args[0] + ":443",
+		Handler: e,
+		TLSConfig: &tls.Config{
+			Certificates: []tls.Certificate{*subCert},
+		},
+	}
+	_ = server.ListenAndServeTLS("", "")
+}
+
+func createSelfSignedCert(certFile, keyFile string) (*tls.Certificate, error) {
+	// 加载根证书和私钥
+	rootCert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load root certificate and key: %v", err)
+	}
+
+	// 解析根证书
+	rootCertParsed, err := x509.ParseCertificate(rootCert.Certificate[0])
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse root certificate: %v", err)
+	}
+
+	// 解析根私钥
+	rootKey, ok := rootCert.PrivateKey.(*rsa.PrivateKey)
+	if !ok {
+		return nil, fmt.Errorf("root private key is not an RSA key")
+	}
+
+	template := x509.Certificate{
+		SerialNumber: big.NewInt(1658),
+		Subject: pkix.Name{
+			Country:            []string{"CN"},
+			Organization:       []string{"mimtproxy"},
+			OrganizationalUnit: []string{"IT Department"},
+			CommonName:         "example.com",
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(10, 0, 0),
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+		IsCA:                  false, // 子证书不是CA证书
+		DNSNames:              []string{"hjm.rebe.capcom.com", "mhws.io", "40912.playfabapi.com"},
+	}
+
+	// 生成子证书的私钥
+	subjectKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate subject key: %v", err)
+	}
+
+	// 用根证书和私钥签发子证书
+	certDERBytes, err := x509.CreateCertificate(rand.Reader, &template, rootCertParsed, &subjectKey.PublicKey, rootKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create certificate: %v", err)
+	}
+
+	// 将证书编码为PEM格式
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDERBytes})
+
+	// 将私钥编码为PEM格式
+	subjectKeyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(subjectKey)})
+
+	// 将PEM格式的证书和私钥加载到tls.Certificate结构体中
+	subCert, err := tls.X509KeyPair(certPEM, subjectKeyPEM)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load sub certificate and key: %v", err)
+	}
+	return &subCert, nil
 }
